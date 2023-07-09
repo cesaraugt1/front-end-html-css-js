1eb6c434461e87a6599dc3ea7fa406b5d77e3181
Cesaraugt1 <cesaraugt1@gmail.com>
2023-07-09 16:23:12 -0300

    feito responsividade do menu e textos da primeira section do HOME

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-// Declarando Variáveis
+//Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 var toggleModal = document.querySelectorAll('.jl-toggle-modal');
+var toggleMenu = document.querySelectorAll('.jl-toggle-menu');
+var menuMobile = document.querySelector('.jl-menu-mob');
+var btnMenuMobIcon = document.querySelector('.jl-btn-menu-mob ion-icon');
 
-// Page Preloader
+//Page Preloader
 window.addEventListener('load', function () {
     var pagePreloder = document.querySelector('.jl-preloader');
     pagePreloder.classList.add('jl-fade-out');
@@ -12,29 +15,48 @@ window.addEventListener('load', function () {
     }, 2000);
 });
 
-// Abrindo e Fechando Informações de Contato
+//Abrindo e Fechando Informações de Contato
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
     this.classList.toggle('jl-change-icon');
 });
 
-// Abrindo e Fechando o Modal de Orcamento
-for (var i = 0; i < toggleModal.length; i++) {
-    toggleModal[i].addEventListener('click', function () {
-        var overlay = document.querySelector('.jl-overlay');
-        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+//Abrindo e Fechando o Menu Mobile
+for (var m = 0; m < toggleMenu.length; m++) {
+        toggleMenu[m].addEventListener('click', function () {
+            var overlay = document.querySelector('.jl-menu-overlay');
+            overlay.classList.toggle('jl-is-open');
+            menuMobile.classList.toggle('jl-menu-is-closed');
+            menuMobile.classList.toggle('jl-menu-is-open');
 
-        overlay.classList.toggle('jl-is-open');
-        modalOrcamento.classList.toggle('jl-is-open');
-        modalOrcamento.classList.toggle('jl-slide-top-in');
+        var icon = btnMenuMobIcon.getAttribute('name');
+
+        if (icon === 'menu') {
+            btnMenuMobIcon.setAttribute('name', 'close');
+        } else {
+            btnMenuMobIcon.setAttribute('name', 'menu');
+        }
 
     });
 }
 
+
+
+//Abrindo e Fechando o Modal de Orcamento
+for (var i = 0; i < toggleModal.length; i++) {
+    toggleModal[i].addEventListener('click', function () {
+        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+        var overlay = document.querySelector('.jl-overlay');
+        overlay.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-slide-top-in');
+    });
+}
+
 var postGallery = document.querySelector('.jl-post-gallery');
-var postGalleryHeight = postGallery.clientHeight;
-postGallery.style.height = (postGalleryHeight - 270) + 'px';
+var portGalleryHeight = postGallery.clientHeight;
+postGallery.style.height = (portGalleryHeight - 270) + 'px';
 
 // Animando Elementos on Scroll com Waypoints
 var myScrollDown = document.querySelector('.jl-scroll-down');
@@ -45,3 +67,6 @@ var waypoint = new Waypoint({
     },
     offset: '80%'
 });
+
+
+
